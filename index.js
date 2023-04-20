10893d23c5ba55f74798e39e16f07253d7ece67d
Juan Lopez <jucalo2195@gmail.com>
2023-04-20 15:18:06 -0500

    Generacion txt

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const port=3000
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
+
 app.post('/formulario',(req,res)=>{
     console.log(req.body);
+    const fs=require('fs');
     const {id,nombre,apellido,titulo,autor,editorial,año}=req.body
     if(!id||!nombre||!apellido||!titulo||!autor||!editorial||!año)return res.redirect('/error.html')
     res.send(`Datos Enviados...${id} - ${nombre} - ${apellido} - ${titulo} - ${autor} - ${editorial} - ${año}`)
+    fs.writeFile('id_123.txt',`${id} - ${nombre} - ${apellido} - ${titulo} - ${autor} - ${editorial} - ${año}`,(error)=>{
+        if(error){
+            console.log(`Error: ${error}`)
+        }
+    })
 })
 
 
